dbb4831949af6778cd694c8d9ff82d2b504a80ce
gangstersso <106763193+gangstersso@users.noreply.github.com>
2022-08-14 01:41:50 +0500

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -17,6 +17,6 @@ printf "\n\e[1;35;47m █░▀█ █ █░▀█ █▄█   \e[0m"
 printf "\n\e[1;35;47m                   \e[0m"
 printf "\n\n\e[3;34;40m Nino юзербот бот нового поколения\e[0m\n\n"
 
-\033[1;32mHikka is running!\033[0m
+\033[1;32mNino is running!\033[0m
 \033[1;32mWeb url: http://localhost:1242\033[0m
 "
